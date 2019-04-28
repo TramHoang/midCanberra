fa1d89cb0b5493ec0d05008f0bca83150a8ae91f
TramHoang <35248953+TramHoang@users.noreply.github.com>
2019-04-28 16:44:20 +1000

    plugin

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1557037255;s:7:"authKey";s:64:"9XVfx4UB?!$w$I8-hnt0>lEf_Db|S{>NL{S1A*ryfVV*>iZERYceKuJrk#3YAttg";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:1116;}
