e8eee4a40048490c0b79f56c3d16481e7c608659
TramHoang <35248953+TramHoang@users.noreply.github.com>
2019-04-28 15:51:15 +1000

    theme

---
+++ wp-content/themes/twentysixteen-child/footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+		</div><!-- .site-content -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">	
+
+			<div class="site-info">
+                <hr>
+				<p>This website was produced by students in the Faculty of Arts & Design, University of Canberra. 
+The text and images reproduced on this site were generously provided by [Canberra House]
+Make text in square brackets a link to this page: http://www.canberrahouse.com.au.
+                </p>
+			</div><!-- .site-info -->
+		</footer><!-- .site-footer -->
+	</div><!-- .site-inner -->
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
